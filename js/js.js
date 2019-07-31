1d3f3234fc933587938509c0487babf63bbe12a2
tales36 <hakuoro36@yahoo.co.jp>
2019-07-31 13:44:58 +0900

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,17 @@
+$(function(){
+	$("ul.gl_menu").hide();
+	$(".menu").click(function(){
+		$(this).toggleClass("menuOpen").next().slideToggle();
+	});
+});
+
+$(function(){
+$('a[href^=#]').click(function(){
+var speed = 500;
+var href= $(this).attr("href");
+var target = $(href == "#" || href == "" ? 'html' : href);
+var position = target.offset().top;
+$("html, body").animate({scrollTop:position}, speed, "swing");
+return false;
+});
+});
